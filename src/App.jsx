f46ce5b6af043fcb7b82d5ad5d856d3d11d7b6b9
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-05-08 15:05:45 +0530

    displayed the data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 function App() {
-  const [value, setVales] = useState([]);
+  const [value, setValues] = useState([]);
 
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzaXZhYmFsYW4uY3MyM0BiaXRzYXRoeS5hYy5pbiIsImV4cCI6MTc3ODIzMjUxOSwiaWF0IjoxNzc4MjMxNjE5LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiYjZkODE0NDMtNDQ3Zi00ZWJmLWE3MGQtZTVlOTE5OTM4NGYxIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic2l2YWJhbGFuIHAiLCJzdWIiOiI1MTRhOTU0My1lNGViLTRiY2YtYmIzMy00NWYwOWQ2ZWE1ZmQifSwiZW1haWwiOiJzaXZhYmFsYW4uY3MyM0BiaXRzYXRoeS5hYy5pbiIsIm5hbWUiOiJzaXZhYmFsYW4gcCIsInJvbGxObyI6IjczNzYyMzFjczMxMCIsImFjY2Vzc0NvZGUiOiJ1S2FKZm0iLCJjbGllbnRJRCI6IjUxNGE5NTQzLWU0ZWItNGJjZi1iYjMzLTQ1ZjA5ZDZlYTVmZCIsImNsaWVudFNlY3JldCI6IkpicG1HVnVKaENqTnZiUUcifQ.A-cF21P1Hdq3llSkEZbdXGjqtynYjMt5kgWNtWjeZas";
-  const get = async () => {
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzaXZhYmFsYW4uY3MyM0BiaXRzYXRoeS5hYy5pbiIsImV4cCI6MTc3ODIzMzU1NiwiaWF0IjoxNzc4MjMyNjU2LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiYzA3NjA4OGItNjQ0My00YTk4LTliNGEtNWU0NTgzMDU3ZjJjIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic2l2YWJhbGFuIHAiLCJzdWIiOiI1MTRhOTU0My1lNGViLTRiY2YtYmIzMy00NWYwOWQ2ZWE1ZmQifSwiZW1haWwiOiJzaXZhYmFsYW4uY3MyM0BiaXRzYXRoeS5hYy5pbiIsIm5hbWUiOiJzaXZhYmFsYW4gcCIsInJvbGxObyI6IjczNzYyMzFjczMxMCIsImFjY2Vzc0NvZGUiOiJ1S2FKZm0iLCJjbGllbnRJRCI6IjUxNGE5NTQzLWU0ZWItNGJjZi1iYjMzLTQ1ZjA5ZDZlYTVmZCIsImNsaWVudFNlY3JldCI6IkpicG1HVnVKaENqTnZiUUcifQ.eVDLeVfCX9oj0GMKM4C616VmiDk4ASaoEJl03MPucnk"
+   const get = async () => {
     const res = await axios.get(
       "http://4.224.186.213/evaluation-service/notifications",
       {
@@ -16,16 +15,21 @@ function App() {
         },
       },
     );
-    setVales(res.data.notifications);
+    setValues(res.data.notifications);
     console.log(res.data.notifications);
   };
 
   const arrange = () => {
+    setValues(
+      value.sort((a, b) => new Date(b.Timestamp) - new Date(a.Timestamp)),
+    );
+
     let dummyarray = [];
     dummyarray = value.filter((u) => u.Type === "Placement");
     dummyarray = [...dummyarray, ...value.filter((u) => u.Type === "Event")];
     dummyarray = [...dummyarray, ...value.filter((u) => u.Type === "Result")];
-    console.log(dummyarray);
+
+    setValues(dummyarray.slice(0,10));
   };
 
   useEffect(() => {
@@ -35,7 +39,14 @@ function App() {
   return (
     <div>
       {value.length > 0 ? (
-        value.map((val, i) => <div>{val.Message}</div>)
+        value.map((val, i) => (
+          <div key={i} className="bars">
+            <div>{i + 1}</div>
+            <div> {val.Message}</div>
+            <div style={{marginLeft : "50px"}}>{val.Type}</div>
+            <div>{val.Timestamp.split(' ')[1]}</div>
+          </div>
+        ))
       ) : (
         <>No Message Now</>
       )}
